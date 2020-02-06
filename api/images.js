865a04e2dc6611685eabd7b1763dbd8ecb612c9c
smlowry <47607332+smlowry@users.noreply.github.com>
2020-02-06 14:54:30 -0600

    Finished sprint 1/ may need to reorganize files

---
+++ api/images.js
@@ -0,0 +1,62 @@
+var multer = require("multer");
+var path = require("path")
+
+var Image = require("../models/image");
+var router = require("express").Router();
+
+// SET STORAGE
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/images')
+  },
+  filename: function (req, file, cb) {
+    cb(null,  file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+  }
+})
+
+// set upload object to store pictures to correct location
+var upload = multer({ storage: storage })
+
+
+// Get list of all images in the database
+router.get("/", function(req, res) {
+    console.log("Getting images");
+    Image.find(function(err, images) {
+        if (err) {
+            res.status(400).send(err);
+        } else {
+            res.json(images);
+        }
+    });
+});
+
+
+// Add a new image to the database
+router.post('/', upload.single('photo'), function(req, res) {
+    
+    if (req.file) {
+        console.log("file: " + req.body.photoName +" saved on.");
+    
+    }
+    else throw 'error';
+    
+    var img = new Image({
+        filename: req.file.filename,
+        photo_name: req.body.photoName,
+        album: req.body.album,
+        upload_date: new Date()
+    });
+    
+    //var image = new Image(req.body);
+    
+    img.save(function(err, img) {
+        if (err) {
+            res.status(400).send(err);
+        } else {
+            //refactor???
+            res.redirect('/home.html');
+        }
+    });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,60 +1,21 @@
+// TO DO: change certain elements on this page such as changing app to router. Move certain elements over to the api/images.js file. Get help. 
+
+
 const express = require('express');
 const multer = require('multer');
 const bodyParser = require('body-parser');
-const path = require('path');
+//removed path
 const Image = require("./models/image");
-
-// SET STORAGE
-var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'public/images')
-  },
-  filename: function (req, file, cb) {
-    cb(null,  file.fieldname + '-' + Date.now() + path.extname(file.originalname))
-  }
-})
-
-// set upload object to store pictures to correct location
-var upload = multer({ storage: storage })
-
 const app = express();
 const PORT = 3000;
 
+const router=express.Router();
+
+
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.post('/upload', upload.single('photo'), (req, res) => {
-    // log the file upload to console
-    if(req.file) {
-        //res.json(req.file);
-        console.log("File: " + req.body.photoName + " saved on.");
-    }
-    else throw 'error';
-    
-    // make a new Image object from the input data
-    var img = new Image({
-        filename: req.file.filename,
-        photo_name: req.body.photoName,
-        album: req.body.album,
-        upload_date: new Date()
-    });
-
-    // save the image to the database
-    img.save(function(err, img) {
-        if (err) {
-            res.status(400).send(err);
-        } else {
-            //res.send("Image was saved.");
-            res.redirect('/home.html');
-        }
-    });
-});
-
-app.get('/getImages', (req,res) =>{
-    Image.find({},(err,img) => {
-               res.json(img);
-               });
-});
+router.use(bodyParser.urlencoded({ extended: true }));
+router.use("/api/images", require("./api/images"));
+app.use(router);
 
 app.listen(PORT, () => {
     console.log('Listening at ' + PORT );
